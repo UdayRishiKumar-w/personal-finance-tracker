31c629a1f96c52b213404b1dee24e57d1becf3dd
GANJI Uday Rishi Kumar <udayrishi.ganji.w@gmail.com>
2025-11-19 17:50:25 +0530

    refactor: add title on toggle theme icon, cache

--- frontend/src/components/ToggleTheme.tsx
+++ frontend/src/components/ToggleTheme.tsx
@@ -7,7 +7,12 @@ import type { FC } from "react";
 const ToggleTheme: FC = () => {
 	const { mode, toggleTheme } = useTheme();
 	return (
-		<IconButton onClick={toggleTheme} color="inherit" aria-label="button to toggle theme between dark and light">
+		<IconButton
+			title={`Switch to ${mode === "dark" ? "light" : "dark"} mode`}
+			onClick={toggleTheme}
+			color="inherit"
+			aria-label="button to toggle theme between dark and light"
+		>
 			{mode === "dark" ? <LightModeIcon aria-label="light" /> : <DarkModeIcon aria-label="dark" />}
 		</IconButton>
 	);
